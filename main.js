aacea9bf5e9f35cf595af12aa903abce04e1f9c1
vedaka1 <vladik.kuleshov.2003@mail.ru>
2022-10-07 19:00:06 +0300

    first

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ function changeColor(){
     let color = document.querySelector('#colorPicker').value;
     document.body.style.backgroundColor = color;
   }
+
+function startTime(){
+    var d=new Date();
+    var h=d.getHours();
+    var m=d.getMinutes();
+    var s=d.getSeconds();
+    m=FullTime(m);
+    s=FullTime(s);
+    document.getElementById('times').innerHTML = h+":"+m+":"+s;
+    t=setTimeout('startTime()',500);
+    }
+    function FullTime(i)
+    {
+    if (i<10){
+    i="0" + i;
+    }
+    return i;
+    }
