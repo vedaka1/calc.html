e1606d95b47d477d39f387bdb65c66603894fe56
vedaka1 <vladik.kuleshov.2003@mail.ru>
2023-04-12 01:28:01 +0300

    date js

--- lab_26/main.js
+++ lab_26/main.js
@@ -1,22 +1,40 @@
 function getName() {
-    let a = document.getElementById('name_field').value;
-    let result =  a.split(' ');
-    a = result[0] + ' ' + result[1];
-    document.getElementById('result_field').value = a;
+    let name = document.getElementById('name_field').value;
+    let result =  name.split(' ');
+    name = result[0] + ' ' + result[1];
     
     let my_date = new Date, 
     my_hours = my_date.getHours(),
-    elements = document.getElementById('time'), 
-    name = a;
+    elements = document.getElementById('time');
 
     switch(true) {
         case (my_hours >= 5) && (my_hours < 11):elements.innerHTML = 'Доброе утро, ' + name;
         break;
         case (my_hours >= 11) && (my_hours < 16):elements.innerHTML = 'Добрый день, ' + name;
         break;
-        case (my_hours >= 16) && (my_hours < 23):elements.innerHTML = 'Добрый вечер, ' + name;
+        case (my_hours >= 16) && (my_hours < 24):elements.innerHTML = 'Добрый вечер, ' + name;
         break;
         case (my_hours >= 0) && (my_hours < 5):elements.innerHTML = 'Доброй ночи, ' + name;
+        break;
+    }
+
+    let today = new Date();
+    let birthDate = document.getElementById('date_field').value;
+    let birthMonth = Number(birthDate.slice(5, 7));
+    let birthDay = Number(birthDate.slice(8, 10));
+    if (birthDate.slice(0, 4) < today.getFullYear()) birthDate = today.getFullYear()+1 + birthDate.slice(4);
+    birthDate = new Date(birthDate.replace(/^(\d+)\.(\d+).(\d+)$/, "$3-$2-$1"));
+    document.getElementById('result_date_field').innerHTML = (Math.floor( (birthDate - today) / 1000 / 60 / 60 / 24)); // количество дней)
+
+    let season = document.getElementById('result_date_field');
+    switch(true) {
+        case (birthMonth == 12) || (birthMonth >= 1) && (birthMonth < 3):season.innerHTML += '<br/>' +'Ты родился ' + birthDay + ' числа' + ', чудесный зимний день';
+        break;
+        case (birthMonth >= 3) && (birthMonth < 7):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ', чудесный весенний день';
+        break;
+        case (birthMonth >= 7) && (birthMonth < 10):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ', чудесный летний день';
+        break;
+        case (birthMonth >= 10) && (birthMonth < 12):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ', чудесный осенний день';
         break;
     }
 }
