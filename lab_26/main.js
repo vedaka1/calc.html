62cc9068e3ad32648fd3e66a8d0d9b46c1dbb0fe
vedaka1 <vladik.kuleshov.2003@mail.ru>
2023-04-12 16:11:49 +0300

    season background

--- lab_26/main.js
+++ lab_26/main.js
@@ -1,7 +1,7 @@
 function getName() {
     let name = document.getElementById('name_field').value;
     let result =  name.split(' ');
-    name = result[0] + ' ' + result[1];
+    name = result[1] + ' ' + result[2];
     
     let my_date = new Date, 
     my_hours = my_date.getHours(),
@@ -22,19 +22,29 @@ function getName() {
     let birthDate = document.getElementById('date_field').value;
     let birthMonth = Number(birthDate.slice(5, 7));
     let birthDay = Number(birthDate.slice(8, 10));
-    if (birthDate.slice(0, 4) < today.getFullYear()) birthDate = today.getFullYear()+1 + birthDate.slice(4);
+
+    if (birthDate.slice(6, 7) < today.getMonth()) {birthDate = today.getFullYear()+1 + birthDate.slice(4)} else {birthDate = today.getFullYear() + birthDate.slice(4)};
     birthDate = new Date(birthDate.replace(/^(\d+)\.(\d+).(\d+)$/, "$3-$2-$1"));
-    document.getElementById('result_date_field').innerHTML = (Math.floor( (birthDate - today) / 1000 / 60 / 60 / 24)); // количество дней)
+    document.getElementById('result_date_field').innerHTML = (Math.floor( (birthDate - today) / 1000 / 60 / 60 / 24) + ' дней до Вашего Дня Рождения!' + '<br/>'); // количество дней)
 
     let season = document.getElementById('result_date_field');
+
     switch(true) {
         case (birthMonth == 12) || (birthMonth >= 1) && (birthMonth < 3):season.innerHTML += '<br/>' +'Ты родился ' + birthDay + ' числа' + ', чудесный зимний день';
+        document.body.style.backgroundImage = "url('images/winter.jpg')";
         break;
-        case (birthMonth >= 3) && (birthMonth < 7):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ', чудесный весенний день';
+        case (birthMonth >= 3) && (birthMonth < 7):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный весенний день';
+        document.body.style.backgroundImage = "url('images/spring.jpg')";
         break;
-        case (birthMonth >= 7) && (birthMonth < 10):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ', чудесный летний день';
+        case (birthMonth >= 7) && (birthMonth < 10):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный летний день';
+        document.body.style.backgroundImage = "url('images/summer.jpg')";
         break;
-        case (birthMonth >= 10) && (birthMonth < 12):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ', чудесный осенний день';
+        case (birthMonth >= 10) && (birthMonth < 12):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный осенний день';
+        document.body.style.backgroundImage = "url('images/autumn.jpg')";
         break;
     }
+}
+
+function image() {
+    document.body.style.backgroundImage = "url('images/autumn.jpg')";
 }
