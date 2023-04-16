d1932e5ab2684b4903f073f77a0ef4ed89b342f9
vedaka1 <vladik.kuleshov.2003@mail.ru>
2023-04-16 23:16:26 +0300

    final

--- lab_26/main.js
+++ lab_26/main.js
@@ -1,23 +1,29 @@
-function getName() {
-    let name = document.getElementById('name_field').value;
-    let result =  name.split(' ');
-    name = result[1] + ' ' + result[2];
-    
+function Name() {
     let my_date = new Date, 
     my_hours = my_date.getHours(),
     elements = document.getElementById('time');
 
     switch(true) {
-        case (my_hours >= 5) && (my_hours < 11):elements.innerHTML = 'Доброе утро, ' + name;
+        case (my_hours >= 5) && (my_hours < 11):elements.innerHTML = 'Доброе утро, ' + localStorage.getItem('username');
         break;
-        case (my_hours >= 11) && (my_hours < 16):elements.innerHTML = 'Добрый день, ' + name;
+        case (my_hours >= 11) && (my_hours < 16):elements.innerHTML = 'Добрый день, ' + localStorage.getItem('username');
         break;
-        case (my_hours >= 16) && (my_hours < 24):elements.innerHTML = 'Добрый вечер, ' + name;
+        case (my_hours >= 16) && (my_hours < 24):elements.innerHTML = 'Добрый вечер, ' + localStorage.getItem('username');
         break;
-        case (my_hours >= 0) && (my_hours < 5):elements.innerHTML = 'Доброй ночи, ' + name;
+        case (my_hours >= 0) && (my_hours < 5):elements.innerHTML = 'Доброй ночи, ' + localStorage.getItem('username');
         break;
     }
 
+}
+
+function getName() {
+
+    let name = document.getElementById('name_field').value;
+    let result =  name.split(' ');
+    name = result[1] + ' ' + result[2];
+
+    if (name != 'undefined undefined') {localStorage.setItem('username', name)};
+    Name();
     let today = new Date();
     let birthDate = document.getElementById('date_field').value;
     let birthMonth = Number(birthDate.slice(5, 7));
@@ -30,21 +36,40 @@ function getName() {
     let season = document.getElementById('result_date_field');
 
     switch(true) {
-        case (birthMonth == 12) || (birthMonth >= 1) && (birthMonth < 3):season.innerHTML += '<br/>' +'Ты родился ' + birthDay + ' числа' + ', чудесный зимний день';
+        case (birthMonth == 12) || (birthMonth >= 1) && (birthMonth < 3):season.innerHTML += '<br/>' +'Ты родился ' + birthDay + ' числа' + ', чудесный зимний день' + '<br/>';
         document.body.style.backgroundImage = "url('images/winter.jpg')";
         break;
-        case (birthMonth >= 3) && (birthMonth < 7):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный весенний день';
+        case (birthMonth >= 3) && (birthMonth < 7):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный весенний день' + '<br/>';
         document.body.style.backgroundImage = "url('images/spring.jpg')";
         break;
-        case (birthMonth >= 7) && (birthMonth < 10):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный летний день';
+        case (birthMonth >= 7) && (birthMonth < 10):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный летний день' + '<br/>';
         document.body.style.backgroundImage = "url('images/summer.jpg')";
         break;
-        case (birthMonth >= 10) && (birthMonth < 12):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный осенний день';
+        case (birthMonth >= 10) && (birthMonth < 12):season.innerHTML += '<br/>' + 'Ты родился ' + birthDay + ' числа' + ', чудесный осенний день' + '<br/>';
         document.body.style.backgroundImage = "url('images/autumn.jpg')";
         break;
     }
+
+    let array = [];
+    let items = document.getElementById('hobby_field').value;
+    if (items != '') {
+        array = items.split(' ');
+        array.sort(function(a, b){
+        let nameA=a.toLowerCase(), nameB=b.toLowerCase()
+        if (nameA < nameB) //сортируем строки по возрастанию  
+            return -1
+        if (nameA > nameB)  
+            return 1
+        return 0 // Никакой сортировки
+    })
+    let len = array.length
+    
+    season.innerHTML += '<br/>' + array + '<br/>' + ' Длина массива: ' + len;
+    }
+
 }
 
-function image() {
+function load() {
     document.body.style.backgroundImage = "url('images/autumn.jpg')";
+    Name();
 }
